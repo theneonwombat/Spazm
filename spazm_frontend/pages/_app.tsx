439c6c44ee6ed2de7a0430cf033c10c40d98af3b
alec <alec.caro@gmail.com>
2022-10-23 12:00:48 -0700

    Makes a backend kind of

--- spazm_frontend/pages/_app.tsx
+++ spazm_frontend/pages/_app.tsx
@@ -1,8 +1,23 @@
+import React from "react";
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const [data, setData] = React.useState(null);
+
+  React.useEffect(() => {
+    fetch("localhost:3001/api")
+      .then((res) => res.json())
+      .then((data) => setData(data.message));
+  }, []);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>{!data ? "Loading..." : data}</p>
+      </header>
+    </div>
+  );
 }
 
 export default MyApp
